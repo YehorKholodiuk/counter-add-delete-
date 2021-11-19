65ddccfd10fcfb80e86abbcd86bb4560544e08c4
ASUS <Yehor Kholodiuk>
2021-11-19 18:36:06 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import {useState} from "react";
+import {nanoid} from 'nanoid';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const initialCounters = [
+    {id: 1, value: 1, buttons: [1]},
+    {id: 2, value: 2, buttons: [1, 2]},
+    {id: 3, value: 3, buttons: [1, 2, 3]},
+]
 
-export default App;
+
+
+function App() {
+
+    const [counters, setCounters] = useState(initialCounters)
+
+    const plusButton = (id, button) => {
+        const newCounters = counters.map(el => el.id === id ? {...el, value: el.value + button} : el)
+        setCounters(newCounters)
+    };
+
+    const minusButton = (id, button) => {
+        const newCounters = counters.map(el => el.id === id ? {...el, value: el.value - button} : el)
+        setCounters(newCounters)
+    };
+
+    const [nextCounterValue, setNextCounterValue] = useState(4)
+
+    const addCounter = () => {
+        const newCounters = [...counters, {id: nanoid(), value: nextCounterValue, buttons: [1, 2, 3, 4]}]
+        setCounters(newCounters)
+        setNextCounterValue(nextCounterValue + 1)
+    };
+
+    const deleteCounter = (id) => {
+        console.log('Delete id', id);
+        const updatedCounters = counters.filter(el => el.id !== id);
+        setCounters(updatedCounters);
+    };
+
+
+    return (
+        <div className="App">
+            {counters.map(el => <div>
+                {el.buttons.map(button => <button onClick={() => minusButton(el.id, button)}>{-button}</button>)}
+
+                {el.value}
+                {el.buttons.map(button => <button onClick={() => plusButton(el.id, button)}>{button}</button>)}
+                <button onClick={() => deleteCounter(el.id)}>Delete counter</button>
+            </div>)}
+
+            <button onClick={addCounter}>Add Counter</button>
+
+        </div>
+    );
+
+}
+    export default App;
